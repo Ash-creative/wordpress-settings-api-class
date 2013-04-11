9e4f6a9eb969a711802f4f944c8ab63ed6a6cc47
Amaury Balmer <amaury@beapi.fr>
2013-04-11 23:24:53 +0200

    add test plugin

---
+++ test-plugin.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ Plugin Name: Settings API Class Test
+ Version: 0.1
+ Plugin URI: https://github.com/herewithme/wordpress-settings-api-class
+ Description: Test all feature of settings API
+ Author: Amaury Balmer
+ Author URI: https://github.com/herewithme/
+ Domain Path: languages
+ Network: false
+ Text Domain: wedevs
+ */
+
+require(dirname(__FILE__) . '/settings-api.php');
